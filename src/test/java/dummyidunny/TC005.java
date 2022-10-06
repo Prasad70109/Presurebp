d5a4769938f691af9188e08200ee89ff503fc1a6
Prasad A C <Prasad A C@prasad>
2022-10-06 07:54:50 +0530

    gjjk

---
+++ src/test/java/dummyidunny/TC005.java
@@ -0,0 +1,13 @@
+package dummyidunny;
+
+public class TC005 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=9;
+		int b=9;
+		System.out.println(a*b);
+
+	}
+
+}
